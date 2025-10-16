300bbaf5de03801af4c8b3eaddac32d7a30e0343
Your Name <your-email@example.com>
2025-10-16 14:01:37 +0530

    layout added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Image from './Image'
+import { Link } from 'react-router-dom';
+// import {Link} from "react-router-dom"
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.png" alt="logo" w={32} h={32}/>
+        <span>TIME12</span>
+      </Link>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+      {/*MOBILE BUTTON*/ }
+        <div className="cursor-pointer text-4xl" onClick={() => setOpen((prev) => !prev)}>
+          {open ? "X" : "≡"}
+        </div>
+        
+        {/* {MOBILE LINK LIST} */}
+        <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 bg-#e6e6ff transition-all ease-in-out ${open ? "-right-0" : "-right-[100%]"}`}>
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+        </a>
+        </div>
+      </div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <a href="">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+        </a>
+      </div>
+    </div>
   )
 }
 
---
+++ src/routes/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div>aagaye guru</div>
+  )
+}
+
+export default LoginPage
