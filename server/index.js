eadae8fc7515a588317933b0962fc74655bb17f2
Your Name <your-email@example.com>
2025-10-31 21:29:53 +0530

    webhook implementation

--- server/index.js
+++ server/index.js
@@ -4,15 +4,30 @@ import postRouter from "./routes/post.route.js";
 import commentRouter from "./routes/comment.route.js";
 import connectDB from "./lib/connectDB.js";
 import dotenv from "dotenv";
+import webHookRouter from "./routes/webhook.route.js";
 
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/posts", postRouter);
 app.use("/comments", commentRouter);
+app.use("/webhooks", webHookRouter);
+
+
+app.use((error, req, res, next)=>{
+
+    res.status(error.status || 500);
+
+    res.json({message: error.message || "Sonething went wrong!",
+    status: error.status,
+    stack: error.stack,
+
+    });
+})
 
 app.listen(3000, ()=>{
     connectDB();
