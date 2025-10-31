089c8d5a7774e0d93dc94b86ebedf858b4cbbbb9
Your Name <your-email@example.com>
2025-10-31 12:46:10 +0530

    database connected

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import connectDB from "./lib/connectDB.js";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.listen(3000, ()=>{
+    connectDB();
+    console.log("server is running!")
+})
